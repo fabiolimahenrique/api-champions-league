679b0b73b74a119b4e7979a83566b4f148784b31
Fabio Lima <fabio.lima.henrique@gmail.com>
2025-08-23 20:02:27 -0300

    Configuração express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("hello world")
+import express, {json} from 'express'
+
+const app = express()
+
+app.use(json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    jogado: "Lima",
+    numero: 10
+  })
+})
+
+const PORTA = process.env.PORT;
+
+app.listen(PORTA, () => {
+   console.log(`Servidor escutando http:localhost:${PORTA}`)
+})
